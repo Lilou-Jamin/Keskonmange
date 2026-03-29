7e5680272d60278f11d857a9f2ec167d58277640
Refragg <francartjeremy51@gmail.com>
2026-03-29 18:10:40 +0200

    Améliorations page profil
    - Améliorations UI
    - Déconnexion
    - Icones pour les menus

--- frontend/keskon-mange/src/pages/profile/Profile.jsx
+++ frontend/keskon-mange/src/pages/profile/Profile.jsx
@@ -1,31 +1,69 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import IconProfile from '../../assets/profile_icon.svg';
+import IngredientsIcon from '../../assets/ingredients_icon.svg';
+import HeartIcon from '../../assets/heart_icon_black.svg';
+import CartIcon from '../../assets/cart_icon.svg';
+import Navbar from '../../components/Navbar.jsx';
+import axios from 'axios';
+import Logo from '../../assets/logo.svg';
 
 export default function Profile() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // On stocke le token et les infos utilisateur
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+
+    // Pour chaque requête vers le backend, on met le token dans le header
+    axios.defaults.headers['authentication'] = null;
+
+    navigate('/');
+  };
+
+  let user;
+  try {
+    user = JSON.parse(localStorage.getItem('user'));
+  } catch {
+    handleLogout();
+  }
+
   return (
     <>
-      <div className="p-4 min-h-screen flex items-center justify-center">
+      <div className="p-4 min-h-screen flex justify-center">
         <div className="text-center">
+          <img src={Logo} alt="Keskon Mange Logo" className="mx-auto mb-8" />
+
           <div className="mb-8 flex flex-row items-center gap-4">
-            <img src={IconProfile} alt="Icône de profil" className="m-auto mb-8" />
-            <h2> Nom - Prénom </h2>
+            <img src={IconProfile} alt="Icône de profil" className="" />
+            <h2> {user.username} </h2>
           </div>
 
-          <div className="mb-8 border-2 border-(--light-grey-color) rounded-lg p-4 flex flex-row items-center gap-4">
-            <Link to="/profile/inventory" className="link">
-              <img src=""></img>
+          <Link to="/profile/inventory" className="link">
+            <div className="mb-4 border-2 border-(--light-grey-color) rounded-lg p-3 flex flex-row items-center gap-4">
+              <img src={IngredientsIcon}></img>
               <h2> Mes ingrédients </h2>
-            </Link>
-          </div>
+            </div>
+          </Link>
 
-          <p>
-            Vous n'avez pas de compte ?{' '}
-            <Link to="/register" className="link">
-              Inscrivez-vous
-            </Link>
-          </p>
+          <Link to="/profile/inventory" className="link">
+            <div className="mb-4 border-2 border-(--light-grey-color) rounded-lg p-3 flex flex-row items-center gap-4">
+              <img src={HeartIcon}></img>
+              <h2> Mes recettes favorites </h2>
+            </div>
+          </Link>
+
+          <Link to="/profile/inventory" className="link">
+            <div className="mb-6 border-2 border-(--light-grey-color) rounded-lg p-3 flex flex-row items-center gap-4">
+              <img src={CartIcon}></img>
+              <h2> Mes ingrédients à racheter </h2>
+            </div>
+          </Link>
+
+          <button onClick={handleLogout}>Déconnexion</button>
         </div>
       </div>
+      <Navbar />
     </>
   );
 }
