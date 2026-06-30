a9fedc7415bb12faa2b813290ab67f0a5454731f
Refragg <francartjeremy51@gmail.com>
2026-06-30 09:29:03 +0200

    Ajout setup Docker pour backend + DB
    - Dockerfile pour le backend
    - Docker compose pour lancer la DB et le backend
    - Fichier env pour Docker compose
    - Ajout de la procédure dans les READMEs
    (la procédure classique sans utiliser Docker reste inchangée)

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,17 @@ app.use('/inventory', require('./routes/inventoryRoutes.js'));
 app.use('/users', require('./routes/userRoutes.js'));
 app.use('/auth', userRoutes);
 
+app.get('/health', (req, res) => {
+  const ip = req.ip;
+  const isLocal = ip === '127.0.0.1' || ip === '::1' || ip === '::ffff:127.0.0.1';
+
+  if (!isLocal) {
+    return res.status(404).end();
+  }
+
+  res.status(200).send('OK');
+});
+
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
